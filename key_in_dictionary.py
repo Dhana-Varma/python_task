2b4a2c3be34dc71bd19b01d84d766ec0576a24cd
Dhananjaya Varma <padmaraju.dhananjaya@gmail.com>
2024-02-16 16:38:40 +0530

    completed tasks

---
+++ key_in_dictionary.py
@@ -0,0 +1,13 @@
+# To Check if a key is already in a dictionary
+my_dict = {1:'a',2:'b', 3:'c'}
+if 2 in my_dict:
+    print("yes key is present")
+else:
+    print("keys is not present")
+
+
+my_dict = {1:'a',20:'b', 3:'c'}
+if 2 in my_dict:
+    print("yes key is present")
+else:
+    print("keys is not present")
